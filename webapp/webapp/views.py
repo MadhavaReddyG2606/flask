9bad8442b77e052f1ce462ae7fd8e30836772533
Naqel123 <madhav.reddy@naqel.com.sa>
2022-10-06 14:09:39 +0300

    test

--- webapp/webapp/views.py
+++ webapp/webapp/views.py
@@ -5,7 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from webapp import app
-#test
+#Test
 @app.route('/')
 @app.route('/home')
 def home():
